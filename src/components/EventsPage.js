d96b3e60fde43ff50cc1f8a37ab1f2cc2e697aeb
soyo-kaze <07sameersaini@gmail.com>
2021-04-23 11:22:23 +0530

    fixed some code..

--- src/components/EventsPage.js
+++ src/components/EventsPage.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 import ReactPlayer from "react-player/youtube";
 import Grid from "@material-ui/core/Grid";
 
@@ -17,131 +17,117 @@ const useStyles = makeStyles({
 });
 
 function EventsPage() {
-    return (
-     <div>
+  const classes = useStyles();
+  return (
+    <div>
       <Grid className="events" container spacing={3}>
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            lg={4}
-            md={4}
-          >
-            <Card className={classes.root} variant="outlined" align="center">
-                <ReactPlayer url='https://youtu.be/o7mES2kTtOs' width='100%' height='100%' />
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        Solution Challenge Info Session
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Grid item xs={12} sm={12} lg={4} md={4}>
+          <Card className={classes.root} variant="outlined" align="center">
+            <ReactPlayer
+              url="https://youtu.be/o7mES2kTtOs"
+              width="100%"
+              height="100%"
+            />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Solution Challenge Info Session
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
 
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            lg={4}
-            md={4}
-          >
-            <Card className={classes.root} variant="outlined" align="center">
-                <ReactPlayer url='https://youtu.be/wAYPxX80v-I' width='100%' height='100%' />
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        Learn to Code a Profile App
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Grid item xs={12} sm={12} lg={4} md={4}>
+          <Card className={classes.root} variant="outlined" align="center">
+            <ReactPlayer
+              url="https://youtu.be/wAYPxX80v-I"
+              width="100%"
+              height="100%"
+            />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Learn to Code a Profile App
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
 
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            lg={4}
-            md={4}
-          >
-            <Card className={classes.root} variant="outlined" align="center">
-                <ReactPlayer url='https://youtu.be/lj5SJBTkmlU' width='100%' height='100%' />
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        Android Study Jams
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Grid item xs={12} sm={12} lg={4} md={4}>
+          <Card className={classes.root} variant="outlined" align="center">
+            <ReactPlayer
+              url="https://youtu.be/lj5SJBTkmlU"
+              width="100%"
+              height="100%"
+            />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Android Study Jams
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
 
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            lg={4}
-            md={4}
-          >
-            <Card className={classes.root} variant="outlined" align="center">
-                <ReactPlayer url='https://youtu.be/qw5RmBU1y7c' width='100%' height='100%' />
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        Android Study Jams | Info Session
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Grid item xs={12} sm={12} lg={4} md={4}>
+          <Card className={classes.root} variant="outlined" align="center">
+            <ReactPlayer
+              url="https://youtu.be/qw5RmBU1y7c"
+              width="100%"
+              height="100%"
+            />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Android Study Jams | Info Session
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
 
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            lg={4}
-            md={4}
-          >
-            <Card className={classes.root} variant="outlined" align="center">
-                <ReactPlayer url='https://youtu.be/5hA25MI3u8Y' width='100%' height='100%' />
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        Hacktoberfest Days
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Grid item xs={12} sm={12} lg={4} md={4}>
+          <Card className={classes.root} variant="outlined" align="center">
+            <ReactPlayer
+              url="https://youtu.be/5hA25MI3u8Y"
+              width="100%"
+              height="100%"
+            />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Hacktoberfest Days
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
 
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            lg={4}
-            md={4}
-          >
-            <Card className={classes.root} variant="outlined" align="center">
-                <ReactPlayer url='https://youtu.be/x3VyDASX7KU' width='100%' height='100%' />
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        DSC: The Beginning
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Grid item xs={12} sm={12} lg={4} md={4}>
+          <Card className={classes.root} variant="outlined" align="center">
+            <ReactPlayer
+              url="https://youtu.be/x3VyDASX7KU"
+              width="100%"
+              height="100%"
+            />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                DSC: The Beginning
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
 
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            lg={4}
-            md={4}
-          >
-            <Card className={classes.root} variant="outlined" align="center">
-                <ReactPlayer url='https://youtu.be/U4xGxQrVp8c' width='100%' height='100%' />
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        GDSC BKBIET
-                    </Typography>
-                </CardContent>
-            </Card>
+        <Grid item xs={12} sm={12} lg={4} md={4}>
+          <Card className={classes.root} variant="outlined" align="center">
+            <ReactPlayer
+              url="https://youtu.be/U4xGxQrVp8c"
+              width="100%"
+              height="100%"
+            />
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                GDSC BKBIET
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-
       </Grid>
-     </div>
-    )
+    </div>
+  );
 }
 
 export default EventsPage;
