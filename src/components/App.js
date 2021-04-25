2ddb38178cd9b91de9e2bc1413170c6c93dbea4a
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-04-25 13:07:59 +0530

    Changes in ContactUs.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,12 @@ import Home from "./Home";
 import Events from "./Events";
 import React from "react";
 
-
 function App() {
   return (
     <div className="App">
-      <ContactUs />
       <Home />
       <Events />
+      <ContactUs />
     </div>
   );
 }
