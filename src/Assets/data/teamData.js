dc0b19f0435e7fd98146bbc9679b1801df516925
Ramakrishna Chhipa <54206397+RKC3000@users.noreply.github.com>
2021-04-25 14:15:15 +0530

    Some updates in Team section

--- src/Assets/data/teamData.js
+++ src/Assets/data/teamData.js
@@ -2,52 +2,66 @@ const data = [
     {
         name: "Sameer Saini",
         position: "DSC Lead",
-        image: "../img/My_picture.jpg",
+        image: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/sameer_saini.jpg",
         iLink: "https://www.instagram.com/sameesaini._/",
         lLink: "https://www.linkedin.com/in/sameer-aka-soyokaze/",
-        tLink: "https://twitter.com/Soyokaz3/",
+        tLink: "https://twitter.com/Soyokaz3/"
     },
     {
         name: "Vaibhav Vyas",
         position: "Tech Lead",
-        image: "../img/My_picture.jpg",
-    
+        image: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/vaibhav_vyas.jpg",
+        iLink: "",
+        lLink: "",
+        tLink: ""
     },
     {
         name: "Mayank Gour",
         position: "DevOps/Cloud Geek",
-        image: "../img/My_picture.jpg",
-    
+        image: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/mayank_gaur_iWHGr3c.jpg",
+        iLink: "",
+        lLink: "",
+        tLink: ""
     },
     {
         name: "Ramakrishna Chhipa",
         position: "Python/ML Lead",
-        image: "../img/My_picture.jpg",
-    
+        image: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/featured_attendees/My%20picture-1.jpg",
+        iLink: "",
+        lLink: "",
+        tLink: ""
     },
     {
         name: "Anshika Gupta",
         position: "Web-Dev/ Social Media Handler",
-        image: "../img/My_picture.jpg",
-    
+        image: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/featured_attendees/anshika_RkO2h8w.jpg",
+        iLink: "",
+        lLink: "",
+        tLink: ""
     },
     {
         name: "Aniket Kumar Sinha",
         position: "Web Dev / UI/UX Designer Lead",
-        image: "../img/My_picture.jpg",
-    
+        image: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/aniket_kumar_sinha_6aCqlxz.jpeg",
+        iLink: "",
+        lLink: "",
+        tLink: ""
     },
     {
         name: "Khushi Tulsiyan",
         position: "Data Science / Chief Editor",
-        image: "../img/My_picture.jpg",
-    
+        image: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/khushi_tulsiyan.jpg",
+        iLink: "",
+        lLink: "",
+        tLink: ""
     },
     {
         name: "Shekar Aggarwal",
         position: "App Dev Lead",
-        image: "../img/My_picture.jpg",
-    
+        image: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_250,q_auto:good,w_250/v1/gcs/platform-data-dsc/avatars/shekhar_aggarwal.jpg",
+        iLink: "",
+        lLink: "",
+        tLink: ""
     }
 ]
 
