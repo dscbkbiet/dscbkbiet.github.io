e84772b239fced79a1158ed65d89d79f60694d01
Restyled.io <commits@restyled.io>
2021-04-30 07:57:12 +0000

    Restyled by prettier

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,30 @@
-import {Link} from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import {makeStyles} from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import { Link } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import React from "react";
 
 const useStyles = makeStyles({
-  root : {
-    maxWidth : 345,
-
+  root: {
+    maxWidth: 345,
   },
-  media : {
-    height : 250,
-
+  media: {
+    height: 250,
   },
 });
 
-const Cards = ({title, content, image, bLink}) => {
-
-
+const Cards = ({ title, content, image, bLink }) => {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
       <Link href={bLink} target="_blank">
         <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={image}
-            title={title}
-          />
+          <CardMedia className={classes.media} image={image} title={title} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               {title}
@@ -45,6 +37,6 @@ const Cards = ({title, content, image, bLink}) => {
       </Link>
     </Card>
   );
-}
+};
 
 export default Cards;
