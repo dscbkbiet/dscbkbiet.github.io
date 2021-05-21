f3301aea1329d1d287ed3966690e1bbcd86dccd1
Ramakrishna Chhipa <54206397+RKC3000@users.noreply.github.com>
2021-05-21 17:20:43 +0530

    Revert "some changes"

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,24 @@
 import "../styles/App.css";
-
-import React from "react";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
-
-import About from "./About";
-import Blog from "./Blog";
-import ContactUs from "./ContactUs";
-import Events from "./Events";
-import EventsPage from "./EventsPage";
-import Footer from "./Footer";
-import Header from "./Header";
 import Home from "./Home";
-import Podcasts from "./Podcasts";
-import PodcastStrip from "./PodcastStrip";
+import Events from "./Events";
 import Team from "./Team";
+import Blog from "./Blog";
+import EventsPage from "./EventsPage";
+import ContactUs from "./ContactUs";
+import Podcasts from "./Podcasts";
+import Header from "./Header";
+import About from "./About";
+import PodcastStrip from './PodcastStrip';
+
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
       <Header />
-      <div
-        style={{
-          height: 80,
-        }}
-      ></div>
+      <div style={{ height: 80 }}></div>
       <Switch>
         <Route path="/dsc-bkbiet-events">
           <EventsPage />
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,52 +1,50 @@
-import { Typography } from "@material-ui/core";
+import React, { Fragment } from "react";
 // import Cards from "./Card";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import React, { Fragment } from "react";
-
+import { makeStyles } from '@material-ui/core/styles';
 import teamData from "../Assets/data/teamData";
-
+import { Typography } from "@material-ui/core";
 import CardTeam from "./CardTeam";
-
-// import "../styles/Team.css";
 
 const useStyles = makeStyles({
   gridContainer: {
-    maxWidth: "193vh",
-    paddingLeft: "40px",
-    paddingRight: "30px",
-    paddingTop: "20px",
-    marginLeft: "auto",
-    marginRight: "auto",
+    paddingLeft: '40px',
+    paddingRight: '30px',
+    paddingTop: '20px',
+    marginLeft: "0px",
+    marginRight: "0px",
     display: "flex",
-    // justify: "space-evenly",
-    alignItems: "center",
+    justify: "space-evenly",
+    alignItems: "center"
   },
   titleName: {
-    paddingTop: "20px",
-  },
+    paddingTop: '20px'
+  }
 });
 
 const Team = () => {
   const classes = useStyles();
-
+  
   return (
-    <div>
+    <Fragment>
       <Typography
         className={classes.titleName}
         variant="h3"
         component="h2"
         color="textSecondary"
         align="center"
+        
+      >Team</Typography>
+      <Grid
+        container
+        spacing={4}
+        className={classes.gridContainer}
       >
-        The Team
-      </Typography>
-      <Grid container spacing={4} className={classes.gridContainer}>
-        {teamData.map((data) => {
+        {teamData.map(data => {
           return (
-            <Grid item xs={12} sm={4} md={3} lg={3}>
+            <Grid item xs={12} sm={4} md={3}>
               <CardTeam
-                name={data.name}
+              name={data.name}
                 position={data.position}
                 iLink={data.iLink}
                 lLink={data.lLink}
@@ -54,11 +52,11 @@ const Team = () => {
                 image={data.image}
                 about={data.about}
               />
-            </Grid>
-          );
+            </Grid>)
         })}
+
       </Grid>
-    </div>
+      </Fragment>
   );
 };
 
