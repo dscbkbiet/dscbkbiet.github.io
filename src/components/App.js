8f00aa3357ccb4b2bd03acff06bdd3c449223fa8
Restyled.io <commits@restyled.io>
2021-05-21 11:51:03 +0000

    Restyled by prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import "../styles/App.css";
 
-import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
 import About from "./About";
 import Blog from "./Blog";
@@ -11,30 +11,42 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Home from "./Home";
 import Podcasts from "./Podcasts";
-import PodcastStrip from './PodcastStrip';
+import PodcastStrip from "./PodcastStrip";
 import Team from "./Team";
 
 function App() {
-  return (<div className = "App"><BrowserRouter><Header />
-          <div style = {{ height: 80 }}>
-          </div>
-      <Switch>
-        <Route path="/dsc - bkbiet - events ">
-           < EventsPage /></Route>
-        <Route path="/Team ">
-                           < Team /></Route>
-        <Route path="/contactUs ">
-                                     < ContactUs />
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div style={{ height: 80 }}></div>
+        <Switch>
+          <Route path="/dsc - bkbiet - events ">
+            <EventsPage />
           </Route>
-        <Route path="/Podcasts ">
-           < Podcasts /></Route>
-        <Route path="/Blog ">
-                         < Blog /></Route>
-        <Route exact path="/">
-                                   < Home /><Events /></Route>
-      </Switch>
-          <About /><PodcastStrip /><Footer /></BrowserRouter>
-    </div>);
+          <Route path="/Team ">
+            <Team />
+          </Route>
+          <Route path="/contactUs ">
+            <ContactUs />
+          </Route>
+          <Route path="/Podcasts ">
+            <Podcasts />
+          </Route>
+          <Route path="/Blog ">
+            <Blog />
+          </Route>
+          <Route exact path="/">
+            <Home />
+            <Events />
+          </Route>
+        </Switch>
+        <About />
+        <PodcastStrip />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
