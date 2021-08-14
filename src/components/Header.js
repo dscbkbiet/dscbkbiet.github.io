25766e660523fe9e25bfdf7e0fe6a89d863d928d
soyo-kaze <07sameersaini@gmail.com>
2021-08-14 18:21:47 +0530

    new beginning

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../styles/Header.css";
-import logo from "../Assets/img/logo.png";
-import {BrowserRouter as Router,Link} from 'react-router-dom';
+import logo from "../Assets/img/logo1.png";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
-const RespMenu = () => { // This function is for making the navbar responsive
+const RespMenu = () => {
+  // This function is for making the navbar responsive
   var x = document.getElementById("myTopnav");
   if (x.className === "header__main") {
-    x.className += " responsive"; //adds this class if the burger icon is clicked 
+    x.className += " responsive"; //adds this class if the burger icon is clicked
   } else {
     x.className = "header__main";
   }
@@ -16,29 +17,41 @@ const RespMenu = () => { // This function is for making the navbar responsive
   } else {
     x.className = "header__menu";
   }
-}
+};
 
 const Header = () => {
   return (
     <>
-		<div className="header__main" id="myTopnav">
-			<div className="resp">
-				<div className="header__logo">
-					<img src={logo} />
-				</div>
-				<div className="header__burger" onClick={RespMenu}>
-					<i class="fa fa-bars"></i>
-				</div>
-			</div>
-			<div className="header__menu" id="myMenu" onClick={RespMenu}>
-				<Link to="/" className="nav__links"><span>Home</span></Link>
-				<Link to="/Team" className="nav__links"><span>Team</span></Link>
-				<Link to="/dsc-bkbiet-events" className="nav__links"><span>Events</span></Link>
-				<Link to="/Blog" className="nav__links"><span>Blogs</span></Link>
-				<Link to="/Podcasts" className="nav__links"><span>Podcasts</span></Link>
-				<Link to="/contactUs" className="nav__links"><span>ContactUs</span></Link>
-			</div>
-		</div>
+      <div className="header__main" id="myTopnav">
+        <div className="resp">
+          <div className="header__logo">
+            <img src={logo} />
+          </div>
+          <div className="header__burger" onClick={RespMenu}>
+            <i class="fa fa-bars"></i>
+          </div>
+        </div>
+        <div className="header__menu" id="myMenu" onClick={RespMenu}>
+          <Link to="/" className="nav__links">
+            <span>Home</span>
+          </Link>
+          <Link to="/Team" className="nav__links">
+            <span>Team</span>
+          </Link>
+          <Link to="/dsc-bkbiet-events" className="nav__links">
+            <span>Events</span>
+          </Link>
+          <Link to="/Blog" className="nav__links">
+            <span>Blogs</span>
+          </Link>
+          <Link to="/Podcasts" className="nav__links">
+            <span>Podcasts</span>
+          </Link>
+          <Link to="/contactUs" className="nav__links">
+            <span>ContactUs</span>
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,64 +3,35 @@ import { makeStyles, Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import { Player, Controls } from "@lottiefiles/react-lottie-player";
 import ReactTypingEffect from "react-typing-effect";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-}));
+import "../styles/Home.css";
 
 function Home() {
-  const classes = useStyles();
-
   return (
     <div>
-      <div className={classes.root}>
-        <Grid container>
-          {/* Content */}
-          <Grid item xs={12} sm={6} lg={6} md={6} align="left">
-            <div className="homeContent">
-              <h1>Google Developer Student Club BKBIET</h1>
-              <h3>
-                Together We{" "}
-                <ReactTypingEffect
-                  text={["Learn", "Build", "Solve", "Transcend"]}
-                  speed={100}
-                  eraseDelay={500}
-                />
-              </h3>
-              <Grid container>
-                <Grid item xs={12} sm={6} lg={4} md={4}>
-                  <a
-                    href="https://discord.gg/7cAesm9H"
-                    className="homeButtonDiscord"
-                  >
-                    Join on Discord
-                  </a>
-                </Grid>
-                <Grid item xs={12} sm={6} lg={4} md={4}>
-                  <a
-                    href="https://dsc.community.dev/accounts/login/?next=/bk-birla-institute-of-engineering-and-technology-pilani/"
-                    className="homeButtonPortal"
-                  >
-                    Join Us
-                  </a>
-                </Grid>
-              </Grid>
-            </div>
-          </Grid>
-          {/* Animation */}
-          <Grid item xs={12} sm={6} lg={6} md={6}>
-            <Player
-              autoplay
-              loop
-              src="https://assets3.lottiefiles.com/packages/lf20_myykup2f.json"
-              style={{ height: "500px", width: "400px" }}
-            >
-              <Controls visible={false} />
-            </Player>
-          </Grid>
-        </Grid>
+      <div className="home__main">
+        {/* Content */}
+        <div className="homeContent">
+          <h1>Google Developer Student Club BKBIET</h1>
+          <h3>
+            Together We{" "}
+            <ReactTypingEffect
+              text={["Learn", "Build", "Solve", "Transcend"]}
+              speed={100}
+              eraseDelay={500}
+            />
+          </h3>
+        </div>
+        {/* Animation */}
+        <div className="home__animation">
+          <Player
+            autoplay
+            loop
+            src="https://assets3.lottiefiles.com/packages/lf20_myykup2f.json"
+            style={{ height: "500px", width: "400px" }}
+          >
+            <Controls visible={false} />
+          </Player>
+        </div>
       </div>
     </div>
   );
