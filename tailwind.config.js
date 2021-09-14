d91a59af10c556261453e5a8abb7ec71a5fb65c7
RKC3000 <ramakrishna.chhipa99@gmail.com>
2021-09-14 17:40:48 +0530

    Added one purge in tailwind config for using tailwind in components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/components",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
